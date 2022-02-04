052385c46f166fb1549e4b3c2b6966fb99218593
AthulyaTS <54679969+AthulyaTS@users.noreply.github.com>
2022-02-04 23:06:51 +0530

    added files

---
+++ djweb/action.js
@@ -0,0 +1,28 @@
+function changeToRed(){
+  document.body.style.backgroundColor = "red";
+}
+
+  function changeToViolet() {
+    document.body.style.backgroundColor = "violet";
+  }
+  
+  function changeToBlue() {
+    document.body.style.backgroundColor = "blue";
+  }
+  
+  function changeToBlack() {
+    document.body.style.backgroundColor = "black";
+  }
+  function changeToYellow() {
+    document.body.style.backgroundColor = "yellow";
+  }
+  function changeToGreen() {
+    document.body.style.backgroundColor = "green";
+  }
+  function changeToOrange() {
+    document.body.style.backgroundColor = "orange";
+  }
+  function changeToBlack() {
+    document.body.style.backgroundColor = "black";
+  }
+  
